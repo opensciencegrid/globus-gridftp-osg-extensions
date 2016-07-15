fed45955c9f13f9cee2d2be49d5dcabf02517f4e
Brian Bockelman <bbockelm@cse.unl.edu>
2016-07-15 13:14:25 -0500

    Add stub for custom command.

--- src/osg_extension_dsi.c
+++ src/osg_extension_dsi.c
@@ -54,7 +54,46 @@ static globus_gfs_storage_iface_t osg_dsi_iface;
 static globus_extension_handle_t osg_dsi_handle = NULL;
 
 static globus_gfs_storage_command_t original_command_function = NULL;
+static globus_gfs_storage_init_t original_init_function = NULL;
 
+enum {
+	GLOBUS_GFS_OSG_CMD_SITE_USAGE = GLOBUS_GFS_MIN_CUSTOM_CMD,
+};
+
+
+static void
+osg_extensions_init(globus_gfs_operation_t op, globus_gfs_session_info_t * session)
+{
+    GlobusGFSName(osg_extensions_init);
+
+    globus_result_t result = globus_gridftp_server_add_command(op, "SITE USAGE",
+                                 GLOBUS_GFS_OSG_CMD_SITE_USAGE,
+                                 3,
+                                 3,
+                                 "SITE USAGE <sp> location: Get usage information for a location.",
+                                 GLOBUS_FALSE,
+                                 GFS_ACL_ACTION_LOOKUP);
+
+    if (result != GLOBUS_SUCCESS)
+    {
+        result = GlobusGFSErrorWrapFailed("Failed to add custom 'SITE USAGE' command", result);
+        globus_gridftp_server_finished_session_start(op,
+                                                 result,
+                                                 NULL,
+                                                 NULL,
+                                                 NULL);
+        return;
+    }
+
+    original_init_function(op, session);
+}
+
+static void
+site_usage(globus_gfs_operation_t op,
+           const char *           path)
+{
+    globus_gridftp_server_finished_command(op, GLOBUS_SUCCESS, "250 Ok\r\n");
+}
 
 static void
 osg_command(
@@ -62,8 +101,20 @@ osg_command(
     globus_gfs_command_info_t *         cmd_info,
     void *                              user_arg)
 {
+    switch (cmd_info->command)
+    {
+    case GLOBUS_GFS_OSG_CMD_SITE_USAGE:
+        site_usage(op, cmd_info->pathname);
+        return;
+    default:
+        // Anything not explicitly OSG-centric is passed to the
+        // underlying module.
+        break;
+    }
+
     original_command_function(op, cmd_info, user_arg);
 }
+
 
 static int
 osg_activate(void)
@@ -96,7 +147,9 @@ osg_activate(void)
 
     memcpy(&osg_dsi_iface, new_dsi, sizeof(globus_gfs_storage_iface_t));
     original_command_function = osg_dsi_iface.command_func;
+    original_init_function = osg_dsi_iface.init_func;
     osg_dsi_iface.command_func = osg_command;
+    osg_dsi_iface.init_func = osg_extensions_init;
 
     globus_extension_registry_add(
         GLOBUS_GFS_DSI_REGISTRY,
